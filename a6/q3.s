6364ae0eea93ab4870f13eecdde8ab3f46905a05
mwang2000 <59301359+mwang2000@users.noreply.github.com>
2020-03-03 01:32:40 -0800

    Add files via upload

--- a6/q3.s
+++ a6/q3.s
@@ -1,14 +1,14 @@
 .pos 0x100
                 ld $0, r4                   # r4 = i = 0
-                ld $n, r6                   # r6 = &n
-                ld (r6), r6                 # r6 = n = n'
-                not r6                      
-                inc r6                      # r6 = -n' 
+                ld $n, r7                   # r7 = &n
+                ld (r6), r7                 # r7 = n = n'
+                not r7                      
+                inc r7                      # r7 = -n' 
                 ld $s, r0                   # r0 = &s
 
         avg:    mov r4, r5                  # r5 = i'
-                add r6, r5                  # r5 = i' - n'
-                beq r5, done                # goto done if i' == n'
+                add r7, r5                  # r5 = i' - n'
+                beq r5, sort                # goto sort if i' == n'
                 mov r4, r5                  # r5 = i'
                 mov r5, r1                  # r1 = i'
                 mov r5, r2                  # r2 = i'
@@ -36,6 +36,7 @@
                 inc r3                      # r3 = -r3
                 add r3,r2                   # r2 = s[i'].average - s[i'+1].average
                 bgt r2, swap                # goto swap if s[i'].average > s[i'+1].average
+                j (r6)
                 
         swap:   ld $6, r4                   # r4 = 6
         L0:     beq r4, L1                  # goto L1 if r4 == 0
@@ -47,10 +48,48 @@
                 dec r4                      # r4 --
                 br L0                       # goto L0
 
-        L1:     
+        L1:     j (r6)
+
+        sort:   mov r7, r2                  # r2 = -n'
+                not r2
+                inc r2                      # r2 = n'
+                dec r2                      # r2 = n' - 1 = i'
+                beq r2, median              # goto median if n'-1 == 0        
+        bubble: ld $0, r3                   # r3 = j' = 0
+                add r3, r4                  # r4 = j' - i'
+                beq r4, end_sort            # goto end_sort if j' == i'
+                mov r3, r4                  # r4 = j'
+                mov r3, r5                  # r5 = j'
+                shl $4, r4                  # r4 = j' * 16
+                shl $3, r5                  # r5 = j' * 8
+                add r5, r4                  # r4 = j' * 24
+                mov r0, r5                  # r5 = &s
+                ld (r5), r5                 # r5 = &base
+                add r5, r1                  # r1 = &s[j']
+                gpc $6, r6                  
+                j compare
+                inc r3                      # j'++
+                br bubble                   # goto bubble
                 
-        bubble: ld $1, r2                   # r2 = 1
-        done:   halt
+        end_sort: dec r2                      # i'--
+                br sort                     # goto sort
+
+        median: not r7                      
+                inc r7                      # r7 = n'
+                shr $1, r7                  # r7 = n'/2
+                inc r7                      # r7 = n'/2 + 1 = m
+                ld (r0), r0                 # r0 = &base
+                mov r7, r1                  # r1 = m'
+                mov r7, r2                  # r2 = m'
+                shl $4, r1                  # r1 = m' * 16
+                shl $3, r2                  # r2 = m' * 8
+                add r2, r1                  # r4 = m' * 24
+                add r1, r0                  # r0 = &s[m']
+                ld (r0), r0                 # r0 = s[m']
+                ld $m, r2                   # r2 = &m
+                st (r0), r2                 # m = s[m']
+
+                halt
 
 .pos 0x200
 n:      .long 3
