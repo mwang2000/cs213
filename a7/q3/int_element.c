68b662d9789fa1c00cfe54799f438c4aeb3a9592
junokims <juno.jaekwon.kim@gmail.com>
2020-03-12 08:34:53 +0000

    most recent

--- a7/q3/int_element.c
+++ a7/q3/int_element.c
@@ -1,46 +1,46 @@
 #include "int_element.h"
+#include "refcount.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "refcount.h"
+#include <string.h>
+#include "element.h"
+#include "list.h"
+#include "tree.h"
+
+/* TODO: Implement all public int_element functions, including element interface functions.
+
+You may add your own private functions here too. */
+
+/* Forward reference to a int_element. You get to define the structure. */
+// struct int_element_class {
+//     void(*compare)(void*, void*);
+//     void(*print)(void*);    
+// };struct str_element_class {
+//     void (*print) (void*);
+//     int (*compare) (void *, void *);
+// };
 
 struct int_element {
-    struct int_element_class* class;
+    struct element_class *class;
     int i;
 };
 
-struct int_element_class {
-    void (*print)(void*);
-    int (*compare) (void *, void *);
-};
 
 void int_element_print(void *thisv) {
-    struct int_element *this = thisv;
-    printf("%d", this->i);
+  struct int_element *this = thisv;
+  printf("%d\n", this->i);
 }
 
- /* Compare two elements. int_element should always compare LESS than str_element.
-   * The compare function should return:
-   *   a number < 0 if the first element is less than the second element,
-   *   0 if the elements compare equal,
-   *   a number > 0 otherwise.
-   */
-int int_element_compare(void *this1, void *this2) {
 
-    // 1. guard =
-    // this1< this2
-    // this1==this2
-    // this1>this2
-
-    // 1
-    // if(is_int_element(this1)){
-
-    // } else {return 1;}
+int int_element_compare(struct element *this1, struct element *this2) {
+    struct int_element *copy1 = this1;
+    struct int_element *copy2 = this2;
 
     if (is_int_element(this1) == 1 && is_int_element(this2) == 0) {
         return -1;
     } else if (is_int_element(this1) == 1 && is_int_element(this2) == 1) {
-        int x = this1->i;
-        int y = this2->i;
+        int x = copy1->i;
+        int y = copy2->i;
         if (x==y) {
             return 0;
         } else if (x < y) {
@@ -49,33 +49,36 @@ int int_element_compare(void *this1, void *this2) {
     } else {
         return 1;
     }
+
 }
 
-struct int_element_class int_element_class = {int_element_print,int_element_compare};
 
-struct int_element* int_element_new(int i) {
-    struct int_element* element = rc_malloc(sizeof (*element), NULL);
-    element -> class = &int_element_class;
-    element -> i = i;
-    return element; 
+struct element_class int_element_class = {int_element_print, int_element_compare};
+
+
+// void int_element_finalizer(void *e) {
+// // struct int_element *temp = e;
+// //    printf("%d\n", temp->i);
+// // free(temp->i); 
+// }
+
+
+
+/* Static constructor that creates new integer elements. */
+struct int_element *int_element_new(int value){
+    struct int_element *obj = rc_malloc(sizeof(struct int_element), NULL);
+    obj->class = &int_element_class;
+    obj->i = value;
+    return obj;
 }
 
-int int_element_get_value(void *element) {
-    struct int_element *int_ = element;
-    return int_->i;
+/* Static function that obtains the value held in an int_element. */
+int int_element_get_value(struct int_element *thisv){
+  struct int_element *this = thisv;
+  return this->i;
 }
 
-int is_int_element(void *element) {
-    struct int_element *int_ = element;
-    if (int_->class == &int_element_class) {
-        return 1;
-    } else {
-        return 0;
-    }
-    // return element->class == &int_element_class
-}
-
-void int_element_finalizer(void *ev) {
-    struct int_element *e = ev;
-    free(e->i); 
+// /* Static function that determines whether this is an int_element. */
+int is_int_element(struct element * thisv){
+    return(thisv->class == &int_element_class);
 }
--- a7/q3/str_element.c
+++ a7/q3/str_element.c
@@ -1,57 +1,70 @@
 #include "str_element.h"
+#include "refcount.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "refcount.h"
+#include <string.h>
+#include "element.h"
+#include "list.h"
+#include "tree.h"
 
+/* TODO: Implement all public str_element functions, including element interface functions.
+
+You may add your own private functions here too. */
+
+/* Forward reference to a str_element. You get to define the structure. */
 struct str_element {
-    struct str_element_class* class;
-    char* string;
+    struct element_class *class;
+    char *i;
 };
 
-struct str_element_class {
-    void (*print) (void*);
-    int (*compare) (void *, void *);
-};
+// void int_element_finalizer(void *e) {
+// // struct int_element *temp = e;
+// //    printf("%d\n", temp->i);
+// // free(temp->i); 
+// }
 
 void str_element_print(void *thisv) {
-    struct str_element *this = thisv;
-    printf("%s\n",this->string);
+  struct str_element *this = thisv;
+  printf("%s\n", this->i);
 }
 
-int str_element_compare(void *this1, void *this2) {
-    if (is_str_element(this1) == 0 && is_str_element(this2) == 1) {
+int str_element_compare(void*thisv, void *thatv) {
+    struct str_element *copy1 = thisv;
+    struct str_element *copy2 = thatv;
+    if (is_str_element(copy1) == 0 && is_str_element(copy2) == 1) {
         return -1;
-    } else if (is_str_element(this1) == 1 && is_str_element(this2) == 1) {
-        return strcmp(this1, this2);
+    } else if (is_str_element(thisv) == 1 && is_str_element(thatv) == 1) {
+        return strcmp(copy1->i, copy2->i);
     } else {
         return 1;
     }
 }
 
-struct str_element_class str_element_class = {str_element_print,str_element_compare};
 
-struct str_element* str_element_new(char* s) {
-    struct str_element* element = rc_malloc(sizeof(*element), str_element_finalizer);
-    element->class = &str_element_class;
-    element->string = s;
-    return element; 
-}
+struct element_class str_element_class = {str_element_print, str_element_compare};
 
-char *str_element_get_value(void* element) {
-    struct str_element *str = element;
-    return str->string;
-}
-
-int is_str_element(void* element) {
-    struct str_element *str = element;
-    if (str->class == &str_element_class) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
 
 void str_element_finalizer(void *ev) {
     struct str_element *e = ev;
-    free(e->string);
+    free(e->i);
+}
+
+/* Static constructor that creates new string elements. */
+struct str_element *str_element_new(char *value){
+    struct str_element *obj = rc_malloc(sizeof(struct str_element), str_element_finalizer);
+    obj->class = &str_element_class;
+    obj->i = strdup(value);
+    return obj;
+}
+
+/* Static function that obtains the string from a str_element. The caller should keep_ref it. */
+char *str_element_get_value(struct str_element *thisv){
+  struct str_element *this = thisv;
+  return this->i;
+}
+
+
+/* Static function that determines whether this is a str_element. */
+int is_str_element(struct element *thisv){
+    return(thisv->class == &str_element_class);
 }
