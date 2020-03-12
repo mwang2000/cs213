a6beab463de4f068179b856953e093bc76c16a48
junokims <juno.jaekwon.kim@gmail.com>
2020-03-12 00:19:11 +0000

    added q2vuln.c

--- a7/q2vuln.c
+++ a7/q2vuln.c
@@ -1,24 +1,28 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-struct string {
+typedef struct string {
     int length;
     char* chars;
-}
+} string;
 
-void main() {
-    char buf[128];
-    char str1;
-    char str2;
-    print(str1);
-    read(0,buf,256);
-    print(str2);
-    write(1,buf,0);
-}
+string str1 = {30, "Welcome! Please enter a name: "};
+string str2 = {11, "Good luck, "};
+string str3 = {43, "The secret phrase is \"squeamish ossifrage\""};
 
-void print(string str) {
-    write(1,str[1],str[0]);
+void print(struct string str) {
+    write(1,str.chars,str.length);
 }
 
 void proof() {
     print(str3);
+}
+
+int main() {
+    char *buf[128];
+    print(str1);
+    int numchar = read(0,buf,256);
+    print(str2);
+    write(1,buf,numchar);
 }
